a4b747dcf431cc3ddae5c113d5531ac134753eb9
Fabio Almeida Lima <fabio.allima@hotmail.com>
2025-01-23 09:34:06 -0300

    Config flyway

--- src/main/java/com/example/dslist/DslistApplication.java
+++ src/main/java/com/example/dslist/DslistApplication.java
@@ -2,8 +2,10 @@ package com.example.dslist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class DslistApplication {
 
 	public static void main(String[] args) {
